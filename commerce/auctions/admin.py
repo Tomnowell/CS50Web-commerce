e1167e1012bf95002c21e5a32ed9428977d90aa7
Tom Nowell <thomnowell@gmail.com>
2021-08-21 16:00:51 +0900

    Add Listing model and create superuser

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -1,3 +1,8 @@
+
 from django.contrib import admin
+from .models import Listing, User
 
 # Register your models here.
+# admin.site.register(modelname)
+admin.site.register(User)
+admin.site.register(Listing)
--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,15 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Listing(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField(blank=True)
+    starting_bid = models.DecimalField(max_digits=8, decimal_places=2)
+    current_top_bid = models.DecimalField(max_digits=8, decimal_places=2)
+    bids = models.IntegerField(blank=True)
+    picture_link = models.CharField(max_length=256, blank=True)
+
+    def __str__(self):
+        return f"{self.name}:{self.current_top_bid}"

dcc5a24eed0d5cb603936abb53dbf06a6987b2b4
Tom Nowell <thomnowell@gmail.com>
2021-08-22 18:50:27 +0900

    Add Bid model and extend User model

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -3,7 +3,10 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    id = models.UUIDField(null=False, blank=False, primary_key=True)
+    first_name = models.CharField(max_length=128, blank=False)
+    last_name = models.CharField(max_length=128, blank=False)
+    avatar = models.URLField(null=True)
 
 
 class Listing(models.Model):
@@ -12,7 +15,21 @@ class Listing(models.Model):
     starting_bid = models.DecimalField(max_digits=8, decimal_places=2)
     current_top_bid = models.DecimalField(max_digits=8, decimal_places=2)
     bids = models.IntegerField(blank=True)
-    picture_link = models.CharField(max_length=256, blank=True)
+    picture_link = models.URLField(null=True)
 
     def __str__(self):
         return f"{self.name}:{self.current_top_bid}"
+
+
+class Bid(models.Model):
+    bidder = models.CharField(max_length=128)
+    item = models.CharField(max_length=128)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __eq__(self, Bid):
+        if self.amount == Bid.amount:
+            return True
+        return False
+
+    def __str__(self):
+        return f"{self.bidder}->{self.item}->{self.amount}"

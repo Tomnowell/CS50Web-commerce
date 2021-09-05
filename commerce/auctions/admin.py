cbda8102446312c8426fb4c09c7594d93167bacb
Tom Nowell <thomnowell@gmail.com>
2021-09-05 12:07:56 +0900

    Fix admin.py

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Listing, User
+from .models import Listing, User, Bid
 
 # Register your models here.
 # admin.site.register(modelname)
 admin.site.register(User)
 admin.site.register(Listing)
-admin.site.register(bid)
+admin.site.register(Bid)

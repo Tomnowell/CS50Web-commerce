882ecb50c391c51898953add4786af73cb999d49
Tom Nowell <thomnowell@gmail.com>
2021-11-06 20:13:20 +0900

    Adjust models.py

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -3,29 +3,31 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    def __str__(self):
+        return f"{self.username}")
 
 
 class Listing(models.Model):
-    name = models.CharField(max_length=128)
-    host = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="listings")
-    description = models.TextField(blank=True)
-    starting_bid = models.DecimalField(max_digits=8, decimal_places=2)
-    current_top_bid = models.DecimalField(max_digits=8, decimal_places=2)
-    number_of_bids = models.IntegerField(blank=True)
-    picture_link = models.URLField(null=True)
+    name=models.CharField(max_length = 128)
+    owner=models.ForeignKey(
+        User, on_delete = models.CASCADE, related_name = "listings")
+
+    description=models.TextField(max_length = 1000)
+    starting_bid=models.DecimalField(max_digits = 8, decimal_places = 2)
+    current_top_bid=models.DecimalField(max_digits = 8, decimal_places = 2)
+    number_of_bids=models.IntegerField(blank = True)
+    image_link=models.URLField(null = True)
 
     def __str__(self):
         return f"{self.name}:{self.current_top_bid}"
 
 
 class Bid(models.Model):
-    bidder = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="item_bids")
-    item = models.CharField(max_length=128)
-    amount = models.DecimalField(max_digits=8, decimal_places=2)
-    date_created = models.DateTimeField(auto_now=True)
+    bidder=models.ForeignKey(
+        User, on_delete = models.CASCADE, related_name = "item_bids")
+    item=models.CharField(max_length = 128)
+    amount=models.DecimalField(max_digits = 8, decimal_places = 2)
+    date_created=models.DateTimeField(auto_now = True)
 
     def __eq__(self, Bid):
         if self.amount == Bid.amount:
@@ -34,3 +36,8 @@ class Bid(models.Model):
 
     def __str__(self):
         return f"{self.bidder}->{self.item}->{self.amount}"
+
+
+class Watchlist(models.Model):
+    user=models.ForeignKey(
+        User, on_delete = models.CASCADE, related_name = "watchlist")

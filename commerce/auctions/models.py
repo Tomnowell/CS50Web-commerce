4065c50e03af646e8e3e631f8d6777dc3c67c69b
Tom Nowell <thomnowell@gmail.com>
2021-12-22 22:21:52 +0900

    change models and urls

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,33 +1,58 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from datetime import datetime, timezone
+
+CATEGORIES = [
+    "Accessories",
+    "Household & Bathroom",
+    "DIY",
+    "Electronics",
+    "Specialist",
+    "Computers & Consoles",
+    "Clothing",
+    "Food & Perishables",
+    "Booze"
+
+]
 
 
 class User(AbstractUser):
     def __str__(self):
-        return f"{self.username}")
+        return f"{self.username}"
 
 
 class Listing(models.Model):
-    name=models.CharField(max_length = 128)
-    owner=models.ForeignKey(
-        User, on_delete = models.CASCADE, related_name = "listings")
+    name = models.CharField(max_length=128)
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="listings")
 
-    description=models.TextField(max_length = 1000)
-    starting_bid=models.DecimalField(max_digits = 8, decimal_places = 2)
-    current_top_bid=models.DecimalField(max_digits = 8, decimal_places = 2)
-    number_of_bids=models.IntegerField(blank = True)
-    image_link=models.URLField(null = True)
+    description = models.TextField(max_length=1000)
+    start_time = models.DateTimeField()
+    start_bid = models.DecimalField(
+        max_digits=8,
+        decimal_places=2)
+    end_time = models.DateTimeField()
+    image_link = models.URLField(null=True)
+    category = models.CharField(choices=CATEGORIES)
+
+    bids = models.ForeignKey(
+        Bid, on_delete=models.DO_NOTHING, related_name="bids")
 
     def __str__(self):
-        return f"{self.name}:{self.current_top_bid}"
+        return f"Listing: {self.name} Owner: {self.owner} Top Bid:{self.current_top_bid}"
+
+    def save(self, *args, **kwargs):
+        self.starting_time = datetime(now)
+        self.ending_time = (self.starting_time + 7)
 
 
 class Bid(models.Model):
-    bidder=models.ForeignKey(
-        User, on_delete = models.CASCADE, related_name = "item_bids")
-    item=models.CharField(max_length = 128)
-    amount=models.DecimalField(max_digits = 8, decimal_places = 2)
-    date_created=models.DateTimeField(auto_now = True)
+    bidder = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="bids")
+    listing = models.ForeignKey(
+        Listing, on_delete=models.CASCADE, related_name="bids")
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    date_created = models.DateTimeField(auto_now=True)
 
     def __eq__(self, Bid):
         if self.amount == Bid.amount:
@@ -35,9 +60,18 @@ class Bid(models.Model):
         return False
 
     def __str__(self):
-        return f"{self.bidder}->{self.item}->{self.amount}"
+        return f"Bidder: {self.bidder} Item: {self.item} Bid: {self.amount}"
 
 
 class Watchlist(models.Model):
-    user=models.ForeignKey(
-        User, on_delete = models.CASCADE, related_name = "watchlist")
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="watchlist")
+
+
+class Review(models.Model):
+    review = models.TextField(max_length=1024)
+    poster = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="user")
+    listing = models.ForeignKey(
+        Listing, on_delete=models.CASCADE, related_name="bids")
+    time = models.DateTimeField(auto_now_add=True)
